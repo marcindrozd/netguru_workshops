8738803312feddfa058a71c89551d8b91cb5e80e
Marcin Drozd <marcindrozd@me.com>
2015-03-20 19:50:44 +0100

    fix ProductsController

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,7 @@
 class ProductsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :require_owner, only: [:edit, :update, :destroy]
+
   expose(:category)
   expose(:products)
   expose(:product)
@@ -46,5 +49,12 @@ class ProductsController < ApplicationController
 
   def product_params
     params.require(:product).permit(:title, :description, :price, :category_id)
+  end
+
+  def require_owner
+    if current_user != product.user
+      flash[:error] = 'You are not allowed to edit this product.'
+      redirect_to category_product_url(category, product)
+    end
   end
 end
