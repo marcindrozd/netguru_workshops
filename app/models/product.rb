517af45133eae3da89e3c3741ca262cd51b03c3a
Marcin Drozd <marcindrozd@me.com>
2015-03-20 18:56:35 +0100

    fix Product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,11 @@ class Product < ActiveRecord::Base
   belongs_to :category
   belongs_to :user
   has_many :reviews
+
+  validates_presence_of :description, :title, :price
+  validates :price, format: { with: /\A\d+(?:\.\d{0,2})?\z/ }, numericality: true
+
+  def average_rating
+    reviews.average(:rating)
+  end
 end
